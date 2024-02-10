75c0291546222aeada3b4b06a4dae3c88558e487
BaggerFast <riosha3@gmail.com>
2024-02-10 21:46:49 +0300

    DELETE: old structure INIT: Api, Domain projects

--- shorty-server/src/main/kotlin/org/elixor/shortyserver/utils/ErrorUtils.kt
+++
@@ -1,7 +0,0 @@
-//package org.elixor.shortyserver.utils
-//
-//import org.springframework.http.HttpStatus
-//import org.springframework.http.ResponseEntity
-//
-//fun genereateErrorResponse(message: String, status: HttpStatus): ResponseEntity<*> =
-//    ResponseEntity(mapOf("error" to message), status)

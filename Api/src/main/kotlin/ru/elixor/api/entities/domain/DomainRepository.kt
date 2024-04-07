2d7be6a3f534cb5fbaf50ff5600c4c7a534323bc
BaggerFast <riosha3@gmail.com>
2024-04-07 14:38:21 +0300

    FIX: domain unique create

--- Api/src/main/kotlin/ru/elixor/api/entities/domain/DomainRepository.kt
+++ Api/src/main/kotlin/ru/elixor/api/entities/domain/DomainRepository.kt
@@ -3,4 +3,6 @@ package ru.elixor.api.entities.domain
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
-interface DomainRepository : JpaRepository<DomainEntity, UUID>
+interface DomainRepository : JpaRepository<DomainEntity, UUID> {
+    fun existsByValue(value: String) : Boolean
+}
--- Api/src/main/kotlin/ru/elixor/api/features/domain/services/DomainServiceImpl.kt
+++ Api/src/main/kotlin/ru/elixor/api/features/domain/services/DomainServiceImpl.kt
@@ -3,16 +3,20 @@ package ru.elixor.api.features.domain.services
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import ru.elixor.api.entities.domain.DomainRepository
+import ru.elixor.api.exceptions.errors.UniqueConflictException
 import ru.elixor.api.features.domain.dto.*
 
 @Service
-class DomainServiceImpl(private val domainRepository: DomainRepository) : DomainService {
-    override fun getAll(): DomainsOutputDtoWrapper = domainRepository.findAll().toWrapperDto()
+class DomainServiceImpl(private val domainRepo: DomainRepository) : DomainService {
+    override fun getAll(): DomainsOutputDtoWrapper = domainRepo.findAll().toWrapperDto()
 
     @Transactional
     override fun create(dto: DomainCreateDto): DomainOutputDto {
+        val domainExists: Boolean = domainRepo.existsByValue(dto.value)
+        if (domainExists) throw UniqueConflictException()
+
         val domain = dto.toEntity()
-        domainRepository.save(domain)
+        domainRepo.save(domain)
         return domain.toDto()
     }
 }
